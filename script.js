cf92fff9da2d70805614e95400f38aee28b3ef48
Amina Magomedova <progristillidan@gmail.com>
2023-04-04 22:16:25 +0300

    rename

--- script.js
+++ script.js
@@ -5,123 +5,42 @@ let HTMLCompletedTodo; // переменная для кода одного об
 let HTMLUncompletedTodo; // переменная для кода одного объекта (невыполненного тодо)
 
 function setTitle() {
-    // object
-    let input = document.querySelector('input');
+    let input = document.querySelector('.inp');
     let todo = {
         name: input.value,
         state: 'uncompleted',
         id: todoObjectList.length+1,
     }
     todoObjectList.push(todo); 
-    
-    // HTMLUncompletedTodo
-    todoUncompletedHTMLcode = [];
-    for (todo of todoObjectList) {  
-        if (todo.state == 'uncompleted') {
-            todoUncompletedHTMLcode.push(`<div class="todoshka">
-    <button class="state" id="${setID(todo.id)}" onclick="complete(${todo.id})">✓</button>
-    <div class="name">${todo.name}</div>
-</div>`);
-        }
-    }   
+    render();
 }
 
 function render() {
-    HTMLUncompletedTodo = '';
-    for (code of todoUncompletedHTMLcode) {
-        HTMLUncompletedTodo += code;
-    }
-    document.getElementById('todo-list').innerHTML=HTMLUncompletedTodo;
+    completedListUpdate();
 }
 
 function setID(id) {
-    return 'button_' + id
+    return 'button_' + id;
 }
 
 function complete(id) {
-    // state
     for (todo of todoObjectList) {
         if (todo.id == id) {
             todo.state = 'completed';
         }
     }
-
-    // HTMLUncompletedTodo
-    todoCompletedHTMLcode = [];
-    for (todo of todoObjectList) {
-        if (todo.state == 'completed') {
-            todoCompletedHTMLcode.push(`<div class="todoshka-comp">
-            <button class="completed-state" id="${setID(todo.id)}" onclick="uncomplete(${todo.id})">✓</button>
-            <s class="name">${todo.name}</s>
-        </div>`);
-        }
-    }
-
-    // render
-    HTMLCompletedTodo = '';
-    for (code of todoCompletedHTMLcode) {
-        HTMLCompletedTodo += code;
-    }
-    document.getElementById('completed-todo-list').innerHTML=HTMLCompletedTodo;
-
-    // delete 
-    todoUncompletedHTMLcode = [];
-    for (todo of todoObjectList) {
-        if (todo.state == 'uncompleted') {
-            todoUncompletedHTMLcode.push(`<div class="todoshka">
-    <button class="state" id="${setID(todo.id)}" onclick="complete(${todo.id})">✓</button>
-    <div class="name">${todo.name}</div>
-</div>`);
-        }
-    }
-    HTMLUncompletedTodo = '';
-    for (code of todoUncompletedHTMLcode) {
-        HTMLUncompletedTodo += code;
-    }
-    document.getElementById('todo-list').innerHTML=HTMLUncompletedTodo;
+    completedListUpdate();
+    uncompletedListUpdate();
 }
 
 function uncomplete(id) {
-    // state
     for (todo of todoObjectList) {
         if (todo.id == id) {
             todo.state = 'uncompleted';
         }
     }
-
-    // HTMLCompletedTodo
-    todoCompletedHTMLcode = [];
-    for (todo of todoObjectList) {
-        if (todo.state == 'completed') {
-            todoCompletedHTMLcode.push(`<div class="todoshka-comp">
-            <button class="completed-state" id="${setID(todo.id)}" onclick="uncomplete(${todo.id})">✓</button>
-            <s class="name">${todo.name}</s>
-        </div>`);
-        }
-    }
-
-    // render
-    HTMLCompletedTodo = '';
-    for (code of todoCompletedHTMLcode) {
-        HTMLCompletedTodo += code;
-    }
-    document.getElementById('completed-todo-list').innerHTML=HTMLCompletedTodo;
-
-    // delete 
-    todoUncompletedHTMLcode = [];
-    for (todo of todoObjectList) {
-        if (todo.state == 'uncompleted') {
-            todoUncompletedHTMLcode.push(`<div class="todoshka">
-    <button class="state" id="${setID(todo.id)}" onclick="complete(${todo.id})">✓</button>
-    <div class="name">${todo.name}</div>
-</div>`);
-        }
-    }
-    HTMLUncompletedTodo = '';
-    for (code of todoUncompletedHTMLcode) {
-        HTMLUncompletedTodo += code;
-    }
-    document.getElementById('todo-list').innerHTML=HTMLUncompletedTodo;
+    uncompletedListUpdate();
+    completedListUpdate();
 }
 
 function enter() {
@@ -139,4 +58,72 @@ function enter() {
 
 function cleanInput() {
     document.getElementById('inp').value = "";
+}
+
+function setIDrename(id) {
+    return 'rename_button_' + id;
+}
+
+function uncompletedListUpdate() {
+    todoCompletedHTMLcode = [];
+        for (todo of todoObjectList) {
+            if (todo.state == 'completed') {
+                todoCompletedHTMLcode.push(`<div class="todoshka-comp">
+                <button class="completed-state" id="${setID(todo.id)}" onclick="uncomplete(${todo.id})">✓</button>
+                <button class="rename" id="${setIDrename(todo.id)}" onclick="reSetName(${todo.id})">✎</button>
+                <s class="name" id="name">${todo.name}</s>
+            </div>`);
+            }
+        }
+
+        HTMLCompletedTodo = '';
+        for (code of todoCompletedHTMLcode) {
+            HTMLCompletedTodo += code;
+        }
+        document.getElementById('completed-todo-list').innerHTML=HTMLCompletedTodo;
+}
+
+function completedListUpdate() {
+    todoUncompletedHTMLcode = [];
+    for (todo of todoObjectList) {
+        if (todo.state == 'uncompleted') {
+            todoUncompletedHTMLcode.push(`<div class="todoshka">
+            <button class="state" id="${setID(todo.id)}" onclick="complete(${todo.id})">✓</button>
+            <button class="rename" id="${setIDrename(todo.id)}" onclick="reSetName(${todo.id})">✎</button>
+            <div class="name" id="name">${todo.name}</div>
+        </div>`);
+        }
+    }
+
+    HTMLUncompletedTodo = '';
+    for (code of todoUncompletedHTMLcode) {
+        HTMLUncompletedTodo += code;
+    }
+    document.getElementById('todo-list').innerHTML=HTMLUncompletedTodo;
+}
+
+function reSetName(id) {
+    for (todo of todoObjectList) {
+        if (todo.id == id) {
+            document.getElementById('rename-todo').innerHTML=`<div class="rename-window">
+            <div class='rename-div'>
+                <p>Enter a name:</p>
+                <input class='rename-inp' id="rename-inp" type='text'>
+                <button class="ok" onclick="rename(${todo.id})">OK</button>
+            </div>
+        </div>`;
+        }
+    }
+    // rename();
+}
+
+function rename(id) {
+    for (todo of todoObjectList) {
+        if (todo.id == id) {
+            todo.name = document.querySelector('.rename-inp').value;
+        }
+    }
+    completedListUpdate();
+    uncompletedListUpdate();
+    document.getElementById('rename-todo').innerHTML='';
 }
